fac3ece5fe5b2ed8f92e49c12473165d53878aa8
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-10-02 15:26:27 -0300

    Comentando e documentando as funcoes nos modulos REST.

--- api/rest.py
+++ api/rest.py
@@ -1,3 +1,14 @@
+"""
+            Módulo REST
+        Nesse módulo vamos usar o framework flask_RESTful para implementar uma API que
+    permita o acesso aos dados.
+        Assim, nesse arquivo vamos implementar os verbos GET,POST,PUT,DELETE
+    e usar as classes DAO já criadas para performar consultas e alterações nos dados do
+    banco de dados da CAWA.
+
+    author:Pedro Henrique Carneiro de Araújo
+"""
 from flask_restful import Resource, request
 from flask import jsonify
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
