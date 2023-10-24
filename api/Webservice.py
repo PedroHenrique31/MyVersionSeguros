ee60297f5b22256888ad57b2572e418345bc6cfd
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-10-24 19:05:05 -0300

    Criada funcionalidade para enviar e quem sabe receber os pdf de apolices! estamos quase acabando o back end.

--- api/Webservice.py
+++ api/Webservice.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,request,send_file
 from flask_restful import Api
 import rest
+import os
 
 app=Flask(__name__)
 api=Api(app)
@@ -8,6 +9,24 @@ api=Api(app)
 api.add_resource(rest.ProdutorRest,'/produtor',endpoint='produtores')
 api.add_resource(rest.ApoliceRest,'/apolice',endpoint='apolices')
 api.add_resource(rest.SeguradoRest,'/segurado',endpoint='segurados')
+
+@app.route('/download_pdf', methods=['GET'])
+def download_pdf():
+    filename = request.args.get('filename')  # Obtém o nome do arquivo da consulta (query parameter)
+
+    # Verifique se o arquivo PDF solicitado existe no diretório
+    pdf_directory = 'C:\\Users\\pedro\\Documents\\Site\\Apolice_Seguros_2023\\'
+    pdf_path = os.path.join(pdf_directory, filename+".pdf")
+
+    print(pdf_path)
+
+    if os.path.exists(pdf_path) and pdf_path.endswith('.pdf'):
+        # Envie o arquivo PDF como resposta
+        return send_file(pdf_path, as_attachment=True)
+    else:
+        return 'Arquivo PDF não encontrado', 404  # Resposta de erro caso o arquivo não seja encontrado
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
